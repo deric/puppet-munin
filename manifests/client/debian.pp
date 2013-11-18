b621f815ab4317d3e5ab1be7825f949006302c8b
Tomas Barton <barton.tomas@gmail.com>
2013-11-18 22:51:49 +0100

    fallback template for not supported codenames on Debian

--- manifests/client/debian.pp
+++ manifests/client/debian.pp
@@ -14,8 +14,12 @@ class munin::client::debian inherits munin::client::base {
     # sarge's munin-node init script has no status
     hasstatus => $hasstatus
   }
-  File['/etc/munin/munin-node.conf']{
-    content => template("munin/munin-node.conf.${::operatingsystem}.${::lsbdistcodename}"),
+  file{'/etc/munin/munin-node.conf':
+    ensure  => file,
+    content => inline_template(file(
+    "munin/munin-node.conf.${::operatingsystem}.${::lsbdistcodename}",
+    "munin/munin-node.conf.${::operatingsystem}",
+    )),
   }
   # workaround bug in munin_node_configure
   plugin { 'postfix_mailvolume': ensure => absent }
